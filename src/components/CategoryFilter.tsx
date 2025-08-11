7656e3895204b4998b2134b79f30b0a170ca5be9
Itamar Nascimento <itamarasa@gmail.com>
2025-08-11 10:15:57 -0300

    Revert "Refactory implement filter category"
    This reverts commit 69d03bab11536ae302a6ccfe5999079863da401c.

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -22,18 +22,6 @@ const CategoryFilter: React.FC<CategoryFilterProps> = ({
 
   return (
     <div className="flex flex-wrap gap-2 justify-center lg:justify-start">
-      <Button
-          key={1}
-          variant={selectedCategory === 'Todos' ? "default" : "outline"}
-          onClick={() => onCategoryChange('Todos')}
-          className={
-            selectedCategory === 'Todos'
-              ? "bg-gradient-to-r from-primary to-blue-500 hover:from-primary/90 hover:to-blue-500/90"
-              : ""
-          }
-        >
-          Todos
-        </Button>
       {categories.map((category) => (
         <Button
           key={category.id}
